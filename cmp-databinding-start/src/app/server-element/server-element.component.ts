cf9edb435ce7a4a2b4e24c604ecec868c46c75df
Andrei Rosu-Cojocaru <andrei.rosucojocaru@gmail.com>
2019-05-01 09:15:35 +0300

    advanced data binding example

---
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html',
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated // None, Native
+})
+export class ServerElementComponent implements OnInit {
+
+  @Input('srvElement') element: { type: string, name: string, content: string }; // this defines the type, not the content => it's a JavaScript object!
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
