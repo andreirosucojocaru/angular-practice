be449feec287fe62f7076d4545b2233be6885dbd
Andrei Rosu-Cojocaru <andrei.rosucojocaru@gmail.com>
2019-05-06 01:35:43 +0300

    http communication

---
+++ http-start/src/app/app.component.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+import { ServerService } from './servers.service';
+import { Response } from '@angular/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  appName = this.serverService.getAppName();
+  servers = [
+    {
+      name: 'Testserver',
+      capacity: 10,
+      id: this.generateId()
+    },
+    {
+      name: 'Liveserver',
+      capacity: 100,
+      id: this.generateId()
+    }
+  ];
+
+  constructor(private serverService: ServerService) { }
+
+  onAddServer(name: string) {
+    this.servers.push({
+      name: name,
+      capacity: 50,
+      id: this.generateId()
+    });
+  }
+
+  onSave() {
+    this.serverService.storeServers(this.servers)
+      .subscribe(
+        (response) => console.log(response),
+        (error) => console.log(error)
+      );
+  }
+
+  onGet() {
+    this.serverService.getServers()
+      .subscribe(
+        (servers: any[]) => {
+          this.servers = servers;
+          console.log(servers);
+        },
+        (error) => console.log(error)
+      );
+  }
+
+
+  private generateId() {
+    return Math.round(Math.random() * 10000);
+  }
+}
